2f55ac5265ff2a962217499eac9170fc0bf3d51b
d0bach <24020398@vnu.edu.vn>
2026-07-21 13:16:52 +0700

    transaction features
    - tạo sơ bộ Transaction và các subclass( Income, Expense)
    - giới hạn input amount > 0.
    - tính hạn chu kỳ tiếp theo mới dừng ở cộng thêm period vào ngày tạo transaction(Expense)

---
+++ src/main/java/core/Expense.java
@@ -0,0 +1,59 @@
+package core;
+
+import java.time.LocalDate;
+
+public class Expense extends Transaction {
+    private String paymentMethod;
+
+    public Expense(int id, double amount, LocalDate date, String note, String category, String wallet, String paymentMethod) {
+        super(id, validateAmount(amount), date, note, category, wallet);
+        this.paymentMethod = paymentMethod;
+    }
+
+    /**
+     * tranh nguoi dung set amount > 0, vi expense la am.
+     * @param amount
+     * @return
+     */
+
+    public static double validateAmount(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Expense amount cannot be negative");
+        }
+        return amount;
+    }
+
+    @Override
+    public void setAmount(double amount) {
+        amount = validateAmount(amount);
+        super.setAmount(amount);
+    }
+
+    @Override
+    public TransactionType getType() {
+        return TransactionType.EXPENSE;
+    }
+
+    @Override
+    public double getSignedAmount() {
+        return -getAmount();
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Payment Method: " + paymentMethod);
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    /**
+     * gioi han nguoi dung set paymentMethod, nhu la QR Code, Cash, Bank Transfer, Credit Card, Debit Card, E-wallet, etc.
+     * @param paymentMethod
+     */
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
